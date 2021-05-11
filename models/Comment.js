23c403d8dbf0089d99567c8061d59d11912b7652
Night Chough <chicken.snuggles@gmail.com>
2021-05-11 12:58:15 -0500

    added comment and post models

--- models/Comment.js
+++ models/Comment.js
@@ -1,19 +1,15 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model { }
+class Comment extends Model { }
 
-Post.init(
+Comment.init(
     {
         id: {
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
             autoIncrement: true,
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
         },
         content: {
             type: DataTypes.STRING,
@@ -28,6 +24,13 @@ Post.init(
             type: DataTypes.INTEGER,
             references: {
                 model: 'user',
+                key: 'id',
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
                 key: 'id',
             }
         },

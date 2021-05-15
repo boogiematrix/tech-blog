598e7bb43de94d3af475ec0b89410073c16e88b5
Night Chough <chicken.snuggles@gmail.com>
2021-05-15 15:12:09 -0500

    added views

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -69,9 +69,22 @@ router.get('/signup', (req, res) => {
     }
 });
 
-router.get('/edit', withAuth, (req, res) => {
-    res.render('edit');
-    return;
+router.get('/edit/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id);
+
+        if (postData) {
+            const post = postData.get({ plain: true });
+
+            res.render('edit', {
+                post,
+            });
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        res.redirect('login');
+    }
 });
 
 router.get('/newpost', withAuth, (req, res) => {
